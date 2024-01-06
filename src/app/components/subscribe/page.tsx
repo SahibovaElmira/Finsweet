e876112f72106314ed9e7ed462587b78f85291ca
SahibovaElmira <sahibovaelmira@gmail.com>
2024-01-07 02:29:13 +0300

    Subscribe_Component_Full

--- src/app/components/subscribe/page.tsx
+++ src/app/components/subscribe/page.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Subscribe: React.FC = () => {
   return (
     <div className="relative w-full lg:w-1280 left-0 lg:left-1 h-auto top-3560 py-8 px-8 bg-blue-700">
       <div className="flex flex-col items-center w-full lg:w-2/3 xl:w-1/2 h-auto mx-auto mb-4">
+        
+      <Image
+        src="/Home/Mikrofon.png"
+        alt="mikrofon"
+        width={80}
+        height={85}
+        style={{ top: '-10px', left: '-10px', position: 'absolute', transform: 'rotate(340deg)' }}
+      />
+
         <p className="font-inter text-2xl font-bold leading-10 text-center mb-4 lg:mb-0">
           Receive new episodes in your inbox.
         </p>
@@ -16,6 +26,13 @@ const Subscribe: React.FC = () => {
           <button className="w-full lg:w-auto h-12 bg-green-500 rounded-md px-4">
             Subscribe
           </button>
+          <Image
+        src="/Home/Mikrofon.png"
+        alt="mikrofon"
+        width={80}
+        height={85}
+        style={{ bottom: '10px', right: '-10px', position: 'absolute', transform: 'rotate(10deg)' }}
+      />
         </div>
       </div>
     </div>
@@ -23,6 +40,7 @@ const Subscribe: React.FC = () => {
 };
 
 export default Subscribe;
+
 
 
 
