3755c0b25b9d4e69db146640e456bc1c708eadad
SahibovaElmira <sahibovaelmira@gmail.com>
2024-01-10 13:13:30 +0300

    PodcastHeader

---
+++ src/app/components/PodcastHeader/page.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+
+const PodcastHeader = () => {
+  return (
+    <div>
+      <div className="relative h-564 bg-cover text-white p-4 md:p-8 text-center md:text-left" style={{ backgroundImage: "url('/Podcast/Podcast1.png')", top: '0px' }}>
+        <div className="w-1440 h-full absolute top-0 left-0" style={{ backgroundImage: "url('/Podcast/Podcast1.png')", backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
+        <div className='bg-blue-700 w-full md:w-96 h-auto md:h-65 py-4 px-8 grid grid-cols-1'>
+          <h1 className="font-bold text-xl md:text-3xl mb-4 py-10">
+            Seçilən
+          </h1>
+          <p className='italic font-inter text-base md:text-lg leading-7 md:leading-8 text-justify'>
+           <span className=' font-extrabold '> UX HAQQINDA</span>  <br/>
+            UX-in məqsədi istifadə etdiyiniz sistemi sadə, problemsiz və asan şəkildə işlətmək, yəni başa düşülən formaya salmaqdır.
+          </p>
+          <Link href="https://www.youtube.com/watch?v=55NvZjUZIO8">
+            <p className='text-center md:text-left md:w-2/5 lg:w-1/4 xl:w-1/5 mt-4 md:mt-8 block text-white font-extrabold italic text-extrabold bg-red-500 border border-red-700 rounded-md p-2'>Ətraflı</p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PodcastHeader;
+
--- src/app/podcasts/page.tsx
+++ src/app/podcasts/page.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import OurSponsor from '../components/OurSponsor/page'
 import Footer from '../components/Footer/page'
 import Copyright from '../components/Copyright/page'
+import PodcastHeader from '../components/PodcastHeader/page'
 
 const podcats = () => {
   return (
     <div>
+      <PodcastHeader/>
     <OurSponsor/>
    <Footer/>
    <Copyright/>
