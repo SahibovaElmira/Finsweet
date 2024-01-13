2f1045cdbe8cfb90708f715fe76e1f1733bf01f6
SahibovaElmira <sahibovaelmira@gmail.com>
2024-01-13 08:17:41 +0300

    BlogFooter

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,12 +3,13 @@ import OurSponsor from '../components/OurSponsor/page'
 import Footer from '../components/Footer/page'
 import Copyright from '../components/Copyright/page'
 import BlogHeader from '../components/BlogHeader/page'
+import Subscribe from '../components/subscribe/page'
 
 const blog = () => {
   return (
     <div>
       <BlogHeader/>
-       <OurSponsor/>
+       <Subscribe/>
       <Footer/>
       <Copyright/>
     </div>
