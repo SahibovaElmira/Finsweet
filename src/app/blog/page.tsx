96d634e4af9553f2c82c9211d15a439b1426b0df
SahibovaElmira <sahibovaelmira@gmail.com>
2024-01-13 08:14:57 +0300

    BlogHeaderComponent

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import OurSponsor from '../components/OurSponsor/page'
 import Footer from '../components/Footer/page'
 import Copyright from '../components/Copyright/page'
+import BlogHeader from '../components/BlogHeader/page'
 
 const blog = () => {
   return (
     <div>
+      <BlogHeader/>
        <OurSponsor/>
       <Footer/>
       <Copyright/>
---
+++ src/app/components/BlogHeader/page.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const BlogHeader = () => {
+  return (
+    <div className="flex ml-3 mt-3">
+  <div className="w-1/2 pr-8">
+    <h2 className="text-blue-700 font-inter text-16 font-semibold ">
+      Seçilən
+    </h2>
+    <p className="font-inter text-32 font-bold ">
+      Layihəyə başlamaq üçün nə etməliyəm?
+    </p>
+    <p className="font-inter text-16 font-normal ">
+      Hər hansı layihənin uğur əldə etməsi ona doğru başlamaqdan asılıdır. Layihəni tərib edərkən ilk olaraq strukturuna diqqət yetirmək lazımdır.
+    </p>
+    <Link href="wwww.google.com">
+    <p className="my-2 text-blue-700">Indi oxu <span>&#8594;</span></p>
+    </Link>
+  </div>
+     
+         <div className="w-1/2">
+        <Image src="/Blog/Blog1.png" alt="Blog1" width={616} height={448} />
+      </div>
+    </div>
+  );
+}
+
+export default BlogHeader;
+
+
