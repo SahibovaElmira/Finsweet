f0b340c8270b0e35a877e4d6162649631a523e9b
SahibovaElmira <sahibovaelmira@gmail.com>
2024-01-14 15:36:50 +0300

    FullResponsiveWebsite!

--- src/app/components/navigation/navbar/Button.tsx
+++ src/app/components/navigation/navbar/Button.tsx
@@ -1,6 +1,9 @@
+import Link from "next/link";
 const Button = () => {
     return (
+      <Link href="https://ru-ru.facebook.com/">
       <button className="h-12 rounded-lg bg-blue-200 font-bold px-5 text-blue-950 ">Daxil olun</button>
+      </Link>
     );
   };
   export default Button;
